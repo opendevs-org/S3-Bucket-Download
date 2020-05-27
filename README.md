8565076c4d2afa1b9d10814847159313e9ec669c
Mihir Kumar <mihirpandey.13@gmail.com>
2020-05-27 11:52:46 +0530

    feat(download.js): aUTO mode and Custom mode added, README, license added
    This commit contains support for Auto Mode & Custom Mode whose demo gif will be added in next
    commit, READE and license files are added

---
+++ README.md
@@ -0,0 +1,63 @@
+<h1 align="center">👋 Welcome to S3 Bucket Download (s3-bucket-download) 🕶</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+  <a href="https://github.com/MiKr13/S3-Bucket-Download/#README" target="_blank">
+    <img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen.svg" />
+  </a>
+  <a href="#" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" />
+  </a>
+  <a href="https://twitter.com/mikr1306" target="_blank">
+    <img alt="Twitter: mikr1306" src="https://img.shields.io/twitter/follow/mikr1306.svg?style=social" />
+  </a>
+</p>
+
+> This project enables downloading data from s3 a breeze, it has 2 modes (auto mode & manual mode) using which you can download s3 bucket files into default or custom path.
+
+### 🏠 [Homepage](https://github.com/MiKr13/S3-Bucket-Download/#README)
+
+### ✨ [Demo](https://github.com/MiKr13/S3-Bucket-Download/#README)
+
+## Install
+
+```sh
+npm install
+```
+
+## Usage
+
+Auto mode:
+
+```sh
+node download --bucketName=comma,seperated,bucketnames --path
+```
+
+Manual mode:
+
+```sh
+node download
+```
+
+## Author
+
+👤 **Mihir Kumar**
+
+* Twitter: [@mikr1306](https://twitter.com/mikr1306)
+* Github: [@MiKr13](https://github.com/MiKr13)
+* LinkedIn: [@mikr13](https://linkedin.com/in/mikr13)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/MiKr13/S3-Bucket-Download/issues).
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+## 📝 License
+
+Copyright © 2020 [Mihir Kumar](https://github.com/mikr13).<br />
+This project is [MIT](https://github.com/MiKr13/S3-Bucket-Download/LICENSE) licensed.
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
--- download.js
+++ download.js
@@ -1,35 +1,121 @@
 /**
- * Configure: ~/.aws/credentials
+ * Configure: ~/.aws/credentials on linux or <%userprofile%>/.aws/credentials on Windows
  * With: aws_access_key_id, aws_secret_access_key
  *
  */
 const argv = require('minimist')(process.argv.slice(2));
-const { exec, spawn } = require('child_process');
+const {
+  exec,
+  spawn
+} = require('child_process');
+const {
+  start,
+  get
+} = require('prompt')
 
-// Check for mandatory parameters
-if (argv.bucket == undefined) {
-    console.log('Please define a --bucket [name]!');
-    return;
+
+const chooseBucketAndPath = () => {
+  const schema = {
+    properties: {
+      bucketName: {
+        description: '[bucket name]/ [bucket name1,bucket name2,etc]',
+        pattern: /^[a-zA-Z0-9.,\-]+$/,
+        message: 'Specify comma separated Bucket names. It must only consist of lowercase letters, numbers, dots (.), comma (,) and hyphens (-)',
+        required: true
+      },
+      path: {
+        description: 'Specify directory where to download the files, if no path provided, files will be downloaded in ./data directory',
+        pattern: /^(.+)\/([^/]+)$/,
+        message: 'Specify directory where to download the files, if no path provided, files will be downloaded in ./data directory',
+        default: './data'
+      }
+    }
+  };
+
+  start();
+
+  get(schema, (err, { bucketName, path }) => {
+    if (err) {
+      console.log(`Prompt error: ${err}`);
+    } else {
+      console.log(`Bucket names to download: ${bucketName.split(',').map(el => el.trim())} ${path}`);
+
+      configure((error, _) => {
+        if (error) {
+          return;
+        } else {
+          initiate(bucketName, path);
+        }
+      });
+    }
+  });
 }
 
-exec(`aws configure set default.s3.max_concurrent_requests 1000 && aws configure set default.s3.max_queue_size 100000`, (error, stdout, stderr) => {
+
+const downloadStart = (bucketName, path) => {
+  const sync = spawn(`aws s3 sync s3://${bucketName} ${path}/${bucketName}`, [], {
+    cwd: __dirname,
+    shell: true
+  })
+
+  sync.stdout.on('data', (data) => {
+    console.log(`${data}`);
+  });
+
+  sync.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  sync.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+}
+
+
+const initiate = (bucketsName, path = './data') => {
+  for (let bucket of bucketsName.split(',').map(el => el.trim())) {
+    downloadStart(bucket, path);
+  }
+};
+
+
+const configure = (callback) => {
+  exec(`aws configure set default.s3.max_concurrent_requests 1000 && aws configure set default.s3.max_queue_size 100000`, (error, stdout, stderr) => {
     if (error) {
-        console.error(`exec error: ${error}`);
-        return;
+      console.error(`exec error: ${error}`);
+
+      callback(error, null);
     }
 
-    const sync = spawn(`aws s3 sync s3://${argv.bucket} ./data`, [], { cwd: __dirname, shell: true })
+    callback(null, true);
+  });
+};
 
-    sync.stdout.on('data', (data) => {
-        console.log(`${data}`);
-    });
 
-    sync.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-    });
+if (argv.bucket === undefined && argv.path === undefined) {
+  console.log('Running in Custom mode!');
 
-    sync.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-    });
-});
+  exec(`aws s3api list-buckets --query "Buckets[].Name"`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
 
+      return;
+    } else if (stderr) {
+      console.error(`AWS CLI error: ${stderr}`);
+    } else {
+      console.log(`Buckets available: ${stdout}`);
+
+      chooseBucketAndPath();
+    }
+  });
+} else {
+  console.log('Running in Auto mode!');
+
+  configure((error, _) => {
+    if (error) {
+      return;
+    } else {
+      initiate(argv.bucket, argv.path);
+    }
+  });
+}
